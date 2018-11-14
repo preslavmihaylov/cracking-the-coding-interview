6bcaacdfd07109cce82d9a68b529fda59080a40e
PreslavMihaylov <preslav@pmihaylov.com>
2018-11-14 21:45:15 +0200

    added stub unit test for stack

--- pstructs/src/stack/tests/test_stack.cpp
+++ pstructs/src/stack/tests/test_stack.cpp
@@ -1,8 +1,34 @@
+#include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestAssert.h>
+
+class StackTest : public CppUnit::TestFixture {
+CPPUNIT_TEST_SUITE(StackTest);
+CPPUNIT_TEST(testAddition);
+CPPUNIT_TEST_SUITE_END();
+
+public:
+    void setUp()
+    {
+    }
+
+    void tearDown()
+    {
+    }
+
+    void testAddition()
+    {
+        CPPUNIT_ASSERT(true);
+    }
+};
 
 int main( int argc, char **argv)
 {
     CppUnit::TextUi::TestRunner runner;
+
+    runner.addTest(StackTest::suite());
     runner.run();
+
     return 0;
 }
