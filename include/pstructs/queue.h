0cc59aa247f820bcaaec842190a1a7e8dbb899fe
PreslavMihaylov <preslav@pmihaylov.com>
2018-12-08 21:12:37 +0200

    included pstructs library

---
+++ include/pstructs/queue.h
@@ -0,0 +1,116 @@
+#ifndef PSTRUCTS_QUEUE_H
+#define PSTRUCTS_QUEUE_H
+
+#include <stdexcept>
+#include <iostream>
+
+#define INIT_CAP 4
+
+namespace pstructs
+{
+    template<class T>
+    class queue
+    {
+        T *data;
+        int head;
+        int tail;
+        int size;
+        int capacity;
+       
+        void copyQueue(const queue& other)
+        {
+            this->size = other.size;
+            this->capacity = other.capacity;
+            this->head = other.head;
+            this->tail = other.tail;
+            
+            this->data = new T[this->capacity];
+            for (int i = 0; i < this->capacity; i++)
+            {
+                this->data[i] = other.data[i];
+            }
+        }
+
+
+        void growQueue()
+        {
+            int newCapacity = this->capacity * 2;
+            T *newData = new T[newCapacity];
+            
+            int newDataIndex = 0;
+            for (int i = this->head; newDataIndex < this->size; i = (i + 1) % this->size)
+            {
+                newData[newDataIndex++] = this->data[i];
+            }
+
+            T *oldData = this->data;
+            this->data = newData;
+            delete[] oldData;
+            
+            this->head = 0;
+            this->tail = this->size;
+            this->capacity = newCapacity;
+        }
+    public:
+        queue() : data(new T[INIT_CAP]), head(0), tail(0), size(0), capacity(INIT_CAP) {}
+        
+        ~queue()
+        {
+            delete[] this->data;
+        }
+
+        queue(const queue& other)
+        {
+            this->copyQueue(other);
+        }
+
+        queue& operator=(const queue& other)
+        {
+            delete[] this->data;
+            this->copyQueue(other);
+
+            return *this;
+        }
+
+        T dequeue()
+        {
+            if (this->size == 0) throw std::invalid_argument("size is zero");
+
+            T element = this->data[this->head];
+            this->head = (this->head + 1) % this->capacity;
+            this->size--;
+
+            return element;
+        }
+
+        void enqueue(T element)
+        {
+            if (this->size == this->capacity) this->growQueue();
+            
+
+            this->data[this->tail] = element;
+            this->tail = (this->tail + 1) % this->capacity;
+
+            this->size++;
+        }
+
+        T peek()
+        {
+            if (this->size == 0) throw std::invalid_argument("size is zero");
+
+            return this->data[this->head];
+        }
+
+        int getSize()
+        {
+            return this->size;
+        }
+
+        bool isEmpty()
+        {
+            return this->size == 0;
+        } 
+    };
+}
+
+#endif
---
+++ include/pstructs/stack.h
@@ -0,0 +1,100 @@
+#ifndef PSTRUCTS_STACK_H
+#define PSTRUCTS_STACK_H
+
+#include <stdexcept>
+
+#define INIT_CAP 4
+
+namespace pstructs
+{
+    template<class T>
+    class stack
+    {
+        T *data;
+        int size;
+        int capacity;
+        
+        void copyStack(const stack& other)
+        {
+            this->size = other.size;
+            this->capacity = other.capacity;
+
+            this->data = new T[this->capacity];
+            for (int i = 0; i < this->capacity; i++)
+            {
+                this->data[i] = other.data[i];
+            }
+        }
+        
+        void growStack()
+        {
+            int newCapacity = this->capacity * 2;
+            T *newData = new T[newCapacity];
+            for (int i = 0; i < this->capacity; i++)
+            {
+                newData[i] = this->data[i];
+            }
+            
+            T *oldData = this->data;
+            this->data = newData;
+            this->capacity = newCapacity;
+
+            delete[] oldData;
+        }
+    public:
+        stack() : data(new T[INIT_CAP]), size(0), capacity(INIT_CAP) {}
+        
+        ~stack() 
+        {
+            delete[] this->data;
+        }
+
+        stack(const stack& other)
+        {
+            this->copyStack(other);
+        }
+
+        stack& operator=(const stack& other)
+        {
+            delete[] this->data;
+            this->copyStack(other);
+
+            return *this;
+        }
+
+        T pop()
+        {
+            if (size <= 0) throw std::invalid_argument("size is zero");
+
+            T element = data[--size];
+
+            return element;
+        }
+
+        void push(T element)
+        {
+            if (size >= this->capacity) this->growStack();
+
+            data[size++] = element;
+        }
+
+        T peek()
+        {
+            if (size <= 0) throw std::invalid_argument("size is zero");
+
+            return data[size - 1];
+        }
+
+        int getSize()
+        {
+            return size;
+        }
+
+        bool isEmpty()
+        {
+            return size == 0;
+        }
+    };
+}
+
+#endif
