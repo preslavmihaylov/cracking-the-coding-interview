3dcfad231750aa4bb57dc19304a8c55a4d0629ba
PreslavMihaylov <preslav@pmihaylov.com>
2019-01-14 10:12:32 +0200

    implemented problem 01 from chapter 04

--- c04-trees-and-graphs/01.route-between-nodes/src/main.cpp
+++ c04-trees-and-graphs/01.route-between-nodes/src/main.cpp
@@ -11,6 +11,7 @@ public:
     Node(uint32_t id) : id(id) {}
 };
 
+bool routeExists(Node& node1, Node& node2);
 bool nodeExists(vector<shared_ptr<Node>>& adjacencyList, uint32_t nodeId);
 shared_ptr<Node> findNode(vector<shared_ptr<Node>>& adjacencyList, uint32_t nodeId);
 void appendNeighbor(vector<shared_ptr<Node>>& adjacencyList, uint32_t nodeId, uint32_t neighborId);
@@ -21,7 +22,37 @@ int main()
 {
     vector<shared_ptr<Node>> adjacencyList;
     readAdjacencyList(adjacencyList);
-    printAdjacencyList(adjacencyList);
+    
+    int node1Id;
+    int node2Id;
+    cin >> node1Id >> node2Id;
+
+    shared_ptr<Node> node1 = findNode(adjacencyList, node1Id);
+    shared_ptr<Node> node2 = findNode(adjacencyList, node2Id);
+    if (node1 == nullptr || node2 == nullptr) return -1;
+    
+    cout << (routeExists(*node1, *node2) ? "YES" : "NO") << endl;
+
+    return 0;
+}
+
+bool routeExists(Node& node1, Node& node2)
+{
+    if (node1.id == UINT_MAX) return false;
+    if (node1.id == node2.id) return true;
+
+    uint32_t tmp = node1.id;
+    node1.id = UINT_MAX;
+    
+    bool pathFound = false;
+    for (auto& neighbor: node1.neighbors)
+    {
+        pathFound = routeExists(*neighbor, node2);
+        if (pathFound) break;
+    }
+
+    node1.id = tmp;
+    return pathFound;
 }
 
 bool nodeExists(vector<shared_ptr<Node>>& adjacencyList, uint32_t nodeId)
