2979817cc72c4598e1ba50c446da880ac5ebb05b
PreslavMihaylov <preslav@pmihaylov.com>
2019-03-05 08:38:52 +0200

    added permutations with dups problem

---
+++ 08.recursion-and-dynamic-programming/08.permutations-with-dups/src/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void permsWithoutDups(vector<string>& results, string& str, int start);
+void swap(string& str, int first, int second);
+
+int main()
+{
+    string str;
+    cin >> str;
+    
+    vector<string> results;
+    permsWithoutDups(results, str, 0);
+    std::sort(results.begin(), results.end());
+    for (string& str : results)
+    {
+        cout << str << endl;
+    }
+}
+
+void permsWithoutDups(vector<string>& results, string& str, int start)
+{
+    if (start >= str.size())
+    {
+        results.push_back(str);
+    }
+    
+    unordered_set<char> visited;
+    for (int i = start; i < str.size(); i++)
+    {
+        if (visited.find(str[i]) != visited.end()) continue;
+        
+        visited.insert(str[i]);
+        swap(str, start, i);
+        permsWithoutDups(results, str, start + 1);
+        swap(str, start, i);
+    }
+}
+
+void swap(string& str, int first, int second)
+{
+    char tmp = str[first];
+    str[first] = str[second];
+    str[second] = tmp;
+}
