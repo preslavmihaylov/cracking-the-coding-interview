fb1778a3beb7212a266457b44ad4ff1f534315eb
PreslavMihaylov <preslav@pmihaylov.com>
2019-03-19 08:58:46 +0200

    refactored sorted search no size problem

--- 10.sorting-and-searching/04.sorted-search-no-size/src/main.cpp
+++ 10.sorting-and-searching/04.sorted-search-no-size/src/main.cpp
@@ -23,7 +23,6 @@ private:
 
 int sortedSearchNoSize(Listy& elements, int term);
 int sortedSearch(Listy& elements, int term, int start, int end);
-int findLength(Listy& elements, int start, int end);
 
 int main()
 {
@@ -49,30 +48,13 @@ int main()
 
 int sortedSearchNoSize(Listy& elements, int term)
 {
-    int length = -1;
-
-    int lastElementIndex = 0;
-    int currentElementIndex = 1;
-    while (length == -1)
+    int length = 1;
+    while (elements[length - 1] != -1 && term > elements[length - 1])
     {
-        int element = elements[currentElementIndex - 1];
-        if (element != -1)
-        {
-            if (term <= element)
-            {
-                return sortedSearch(elements, term, lastElementIndex, currentElementIndex);
-            }
-
-            lastElementIndex = currentElementIndex - 1;
-            currentElementIndex *= 2;
-        }
-        else
-        {
-            length = findLength(elements, lastElementIndex, currentElementIndex - 1);
-        }
+        length *= 2;
     }
 
-    return sortedSearch(elements, term, lastElementIndex, length);
+    return sortedSearch(elements, term, length / 2, length);
 }
 
 int sortedSearch(Listy& elements, int term, int start, int end)
@@ -84,32 +66,13 @@ int sortedSearch(Listy& elements, int term, int start, int end)
     {
         return mid;
     }
-    else if (elements[mid] > term)
+    else if (elements[mid] > term || elements[mid] == -1)
     {
         return sortedSearch(elements, term, start, mid);
     }
     else
     {
         return sortedSearch(elements, term, mid + 1, end);
-    }
-}
-
-int findLength(Listy& elements, int start, int end)
-{
-    if (start >= end) return 0;
-
-    int mid = (start + end) / 2;
-    if (elements[mid] != -1 && elements[mid + 1] == -1)
-    {
-        return mid + 1;
-    }
-    else if (elements[mid] == -1)
-    {
-        return findLength(elements, start, mid);
-    }
-    else // if (elements[mid] != -1)
-    {
-        return findLength(elements, mid, end);
     }
 }
 
